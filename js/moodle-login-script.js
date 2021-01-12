d1d9d974373bbe91ad47cb8c4ccb29a83081b397
AtanasGruev <atanas.gruev.98@gmail.com>
2021-01-12 20:23:47 +0200

    added folders

---
+++ js/moodle-login-script.js
@@ -0,0 +1,26 @@
+(function(){
+    var loginButton = document.getElementById("mlogin-button");
+    loginButton.addEventListener("click", loginUser);
+})();
+
+function loginUser(event) {
+    // Забраняваме презареждането, освен ако нямаме success.
+    // Тогава отиваме в default view за нашия moodle.
+    event.preventDefault();
+        
+    // вход - Валидираме username и password
+    const userName = document.querySelector("[name=mlogin-username]");
+    const password = document.querySelector("[name=mlogin-password]");
+
+    // Ако възникнат проблеми с валидацията, <div> с id = mlogin-errors
+    var errorDiv = document.getElementById("mlogin-errors");
+    errorDiv.innerHTML = "";
+
+    // [TODO]
+    // Валидацията -- трябва ни достъп до базата данни.
+    // Сигурно пак най-лесно с json файл и асинхранна заявка към него.
+    // Лесно -- има ли такъв username, асоциран с такава парола.
+
+    userName.value = "";
+    password.value = "";
+}
