52fcf24b56460e49b2547a6ff0abf058361c7d35
lyubo anin <lyubo.anin@gmail.com>
2021-01-26 16:21:21 +0200

    modal improvement and visibility changes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
+//const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
-   
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-    })
-})
+
+
+const modalOnLoad = document.getElementById("modal")
+// window.onload = openModal(modalOnLoad)
+
+
+//отварянето го преместих в callback функцията, демек отваря се след като е load-нат json-а и е променен innerHTML-a на модала
+//setTimeout(() => { openModal(modalOnLoad) }, 2000);
+
+//закоментираното вече е излишно май, тъй като няма да имаме бутон който да отваря модалите
+
+// openModalButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = document.querySelector(button.dataset.modalTarget)
+//         openModal(modal)
+//     })
+// })
 
 overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active')
     modals.forEach(modal => {
-    closeModal(modal)
+        closeModal(modal)
     })
 })
 
 closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
+        const modal = button.closest('.modal')
+        closeModal(modal)
     })
 })
 
@@ -33,6 +43,12 @@ function closeModal(modal) {
     if (modal == null) return
     modal.classList.remove('active')
     overlay.classList.remove('active')
+
+    //засега тук, може и да е ненужно
+    var moodle = document.getElementById('moodle-container')
+    moodle.style.visibility="visible";
+
+
 }
 
 function printMessage(message) {
@@ -55,16 +71,16 @@ function incrementActions(counterActionsObj) {
         for (var i = 0; i < divsActions.length; i += 1) {
             divsActions[i].style.backgroundColor = "#39d078";
             divsActions[i].style.boxShadow = "none";
-          }
-          counterActionsObj["flag"] = true;
-    } else if(counterActionsObj["flag"]) {
+        }
+        counterActionsObj["flag"] = true;
+    } else if (counterActionsObj["flag"]) {
         var currAction = document.getElementById("action-" + (counterActionsObj["counterActions"]) % 10);
         currAction.style.backgroundColor = "blue";
         currAction.style.boxShadow = "0 0 5px red";
         counterActionsObj["counterActions"]++;
         console.log(counterActionsObj["counterActions"]);
 
-        if(counterActionsObj["counterActions"] % 10 == 0) {
+        if (counterActionsObj["counterActions"] % 10 == 0) {
             counterActionsObj["flag"] = false;
         }
     }
@@ -72,11 +88,32 @@ function incrementActions(counterActionsObj) {
 
 function parseWeeks() {
     fetch("../scenarios/week-0.json")
-    .then(data => data.json())
-    .then(result => console.log(result))
+        .then(data => data.json())
+        .then(result => callback(result))
 }
 
-(function(){
+
+
+function callback(result) {
+    //console.log(result)
+    var modalBody = document.getElementsByClassName("modal-body")[0]
+    console.log(modalBody.innerHTML)
+    console.log(result[0].event)
+    modalBody.innerHTML = result[0].event
+    setTimeout(() => { openModal(modalOnLoad) }, 1500);
+
+    //не знам дали тук е възможно да бъде цялата логика за week0
+    //ако е в някакъв цикъл и итерира през евентите, може ли цикъла да блокира до натискане на опция на модала
+    //edit: или до успешното регистриране и логване в мудъл
+    //дали ще е по-лесно на бъдат две отделни функции - в едната е първата половина на week0 - която казва да се 
+    //регистрираш и може да има още едно модал който да обяснява нещо за статистиките когато успееш да се регистрираш
+    //и втората половина е вече тази в която имаме опциите дето трябва да избираш
+
+    
+}
+
+
+(function () {
     // Да се направи на отделна функция, а IIFE да служи за main()
 
     var toggleMode = document.getElementById("toggle-day-night");
@@ -85,11 +122,11 @@ function parseWeeks() {
     toggleMode.onclick = () => {
         document.querySelector("body").classList.toggle("active");
         console.log(counterToggle);
-        if(counterToggle % 2 == 0) {
+        if (counterToggle % 2 == 0) {
             // Добавяме рамка в night mode
             var iframeVar = document.getElementById("moodle-iframe").contentWindow;
             var moodleRoot = iframeVar.document.getElementById("root");
-            
+
             moodleRoot.style.border = "2px solid #FDD835";
             moodleRoot.style.borderRadius = "0.5rem";
 
@@ -97,7 +134,7 @@ function parseWeeks() {
             var targetAddress = currAddress.substring(0, currAddress.lastIndexOf('/'));
 
             // Нуждаем се от код, който да прави видимо съдържанието на moodle-main
-            if(iframeVar.location.href === targetAddress + "/moodle-main.html") {
+            if (iframeVar.location.href === targetAddress + "/moodle-main.html") {
                 moodleRoot.style.color = "#FDD835";
                 moodleRoot.style.backgroundColor = "#09071E";
             }
@@ -105,14 +142,14 @@ function parseWeeks() {
             // При последващ toggle, махаме рамката
             var iframeVar = document.getElementById("moodle-iframe").contentWindow;
             var moodleRoot = iframeVar.document.getElementById("root");
-            
+
             moodleRoot.style.border = "2px solid black";
             moodleRoot.style.color = "black";
-            
+
             var currAddress = iframeVar.location.href;
             var targetAddress = currAddress.substring(0, currAddress.lastIndexOf('/'));
 
-            if(iframeVar.location.href === targetAddress + "/moodle-main.html") {
+            if (iframeVar.location.href === targetAddress + "/moodle-main.html") {
                 moodleRoot.style.border = "2px solid black";
                 moodleRoot.style.backgroundColor = "white";
             }
@@ -120,9 +157,14 @@ function parseWeeks() {
         counterToggle++;
     }
 
+
+    var counterActionsObj = {counterActions : 0, flag: true};
+    var actionBtn = document.getElementById("btn-actions");
+    actionBtn.addEventListener("click", function() {
+        incrementActions(counterActionsObj)
+    });
+
+
     parseWeeks();
 
-    //var message = "A semester begins at FMI!"
-    //setInterval(printMessage, 500, message);
-    // setTimeout(printMessage, 5000, message);
 })();
